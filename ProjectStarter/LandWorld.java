17aa0403590bbdba8dfd8e37dd8c2e53b44b9d19
zaarailias <thelemonadegirl786@gmail.com>
2025-09-17 09:33:32 -0500

    Add files via upload

---
+++ ProjectStarter/LandWorld.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class LandWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import mayflower.*;
+public class LandWorld extends MyWorld
+{
+    
+    private boolean start;
+    public LandWorld()
+    {
+
+        super("img/BG/image.jpg", new String[32][6]);
+        
+        showText("Replace this bg with a sky image", 200, 200, Color.BLACK);
+        
+        
+    }
+    public boolean isStarted(){
+        if (Mayflower.isKeyDown( Keyboard.KEY_RIGHT )) {
+            start = true;
+            
+        }
+        else
+        {
+            start = false;
+        }
+        return start;
+    } 
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+}
