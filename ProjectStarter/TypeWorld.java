b4c14cb6dc1280618e3f563fa52fd8f553f357cd
Shriya Nair <shriyanair323@gmail.com>
2025-09-17 10:09:11 -0500

    Enum of different world types

---
+++ ProjectStarter/TypeWorld.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class TypeWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public enum TypeWorld
+{
+    // instance variables - replace the example below with your own
+    Sky, Land, Water 
+}
