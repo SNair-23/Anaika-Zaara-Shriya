c0fdee5f1f5edc2d17fe6139c542a1812222af63
zaarailias <thelemonadegirl786@gmail.com>
2025-10-01 10:29:21 -0500

    Add files via upload

---
+++ ProjectStarter/EndWinScreen.java
@@ -0,0 +1,37 @@
+import mayflower.*;
+/**
+ * Write a description of class EndWinScreen here.
+ *
+ * @author (Zaara I)
+ * @version (a version number or a date)
+ */
+public class EndWinScreen extends MyWorld
+{
+    private String text;
+    private boolean start;
+    private MayflowerImage startbutton;
+  
+    public EndWinScreen()
+    {
+        super();
+        text = "You win!";
+        showText(this.text, 200, 100, Color.WHITE);
+        
+       
+    }
+    public boolean isEnded()
+    {
+        if (seaWorld = 10)
+        {
+            display EndWinScreen;
+        }
+        else
+        {
+            display EndLoseScreen;
+        }
+    }
+    public void act()
+    {
+        
+    }
+}
